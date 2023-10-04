fc80f4de4a00ca72338c3af520d785e01193bbd1
Nursat2022 <nursatsakyshev2003@gmail.com>
2023-10-04 10:37:37 +0600

    Adding calendar

--- EventCalendar/ViewController.swift
+++ EventCalendar/ViewController.swift
@@ -12,9 +12,41 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        view.backgroundColor = .white
+        view.backgroundColor = UIColor(red: 246/255, green: 246/255, blue: 246/255, alpha: 1)
+        self.navigationItem.title = "Event Calendar"
+        self.navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Add", style: .plain, target: self, action: #selector(add))
+        
+        createCalendar()
     }
+    
+    func createCalendar() {
+        let calendarView = UICalendarView()
+        calendarView.translatesAutoresizingMaskIntoConstraints = false
+        
+        calendarView.calendar = .current
+        calendarView.locale = .current
+        calendarView.fontDesign = .rounded
+        calendarView.delegate = self
+        calendarView.backgroundColor = .systemBackground
+        
+        view.addSubview(calendarView)
+        
+        NSLayoutConstraint.activate([
+            calendarView.topAnchor.constraint(equalTo: view.topAnchor),
+            calendarView.rightAnchor.constraint(equalTo: view.rightAnchor),
+            calendarView.leftAnchor.constraint(equalTo: view.leftAnchor),
+            calendarView.heightAnchor.constraint(equalToConstant: 550),
+        ])
+    }
+    
+    @objc func add() {
+        print("add")
+    }
+}
 
-
+extension ViewController: UICalendarViewDelegate {
+    func calendarView(_ calendarView: UICalendarView, decorationFor dateComponents: DateComponents) -> UICalendarView.Decoration? {
+        return nil
+    }
 }
 
