10863f221331bf17af13eaba0c3f54f6ca92179d
Nursat2022 <nursatsakyshev2003@gmail.com>
2023-10-04 09:22:10 +0600

    Initial Commit

---
+++ EventCalendar/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  EventCalendar
+//
+//  Created by Nursat Sakyshev on 04.10.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
