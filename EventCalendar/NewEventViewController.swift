61d3013eb5e0d8496cc583a7eaec84b7632b75ea
Nursat2022 <nursatsakyshev2003@gmail.com>
2023-10-05 23:00:00 +0600

    date picker

--- EventCalendar/NewEventViewController.swift
+++ EventCalendar/NewEventViewController.swift
@@ -8,11 +8,61 @@
 import UIKit
 
 class NewEventViewController: UIViewController {
-
+    
+    let eventNameTextField: CustomTextField = {
+        let textField = CustomTextField()
+        textField.placeholder = "Event name"
+        return textField
+    }()
+    
+    let EventDate: UIDatePicker = {
+        let datePicker = UIDatePicker()
+        datePicker.addTarget(self, action: #selector(datePickerValueChanged(_:)), for: .valueChanged)
+//        datePicker.locale = Locale(identifier: "ru_RU")
+        datePicker.locale = Locale(identifier: "en_US_POSIX")
+//        datePicker.isUserInteractionEnabled = true
+        return datePicker
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        view.backgroundColor = .white
+        [eventNameTextField].forEach {
+            $0.delegate = self
+        }
+        
+        self.navigationItem.title = "New Event"
+        self.navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Add", style: .plain, target: self, action: #selector(add))
+        
+        eventNameTextField.translatesAutoresizingMaskIntoConstraints = false
+        [eventNameTextField, EventDate].forEach {
+            $0.translatesAutoresizingMaskIntoConstraints = false
+            view.addSubview($0)
+        }
+        
+        NSLayoutConstraint.activate([
+            eventNameTextField.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            eventNameTextField.rightAnchor.constraint(equalTo: view.rightAnchor, constant: -10),
+            eventNameTextField.leftAnchor.constraint(equalTo: view.leftAnchor, constant: 10),
+            eventNameTextField.heightAnchor.constraint(equalToConstant: 50),
+            
+            EventDate.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            EventDate.centerYAnchor.constraint(equalTo: view.centerYAnchor)
+        ])
+    }
+    
+    @objc func add() {
         
     }
+    
+    @objc func datePickerValueChanged(_ datePicker: UIDatePicker) {
+    }
 
+}
+
+extension NewEventViewController: UITextFieldDelegate {
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        self.view.endEditing(true)
+        return false
+    }
 }
