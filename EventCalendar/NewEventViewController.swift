798d6d1a1bf1cd23fdb6d2d3375d5246be289a99
Nursat2022 <nursatsakyshev2003@gmail.com>
2023-10-05 21:41:54 +0600

    new event controller

---
+++ EventCalendar/NewEventViewController.swift
@@ -0,0 +1,18 @@
+//
+//  NewEventViewController.swift
+//  EventCalendar
+//
+//  Created by Nursat Sakyshev on 05.10.2023.
+//
+
+import UIKit
+
+class NewEventViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+
+}
